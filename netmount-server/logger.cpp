46ad5d1a62c9217401e24cb6ae2dc55b2a9c83c1
Jaroslav Rohel <jaroslav.rohel@gmail.com>
2025-07-07 08:32:33 +0200

    server: Add ISO 8601 timestamps (ms precision) to log messages
    This improves the ability to analyze issues related to transfer
    latency or performance.

--- netmount-server/logger.cpp
+++ netmount-server/logger.cpp
@@ -3,4 +3,25 @@
 
 #include "logger.hpp"
 
+#include <array>
+#include <chrono>
+#include <cstdio>
+
+constexpr auto LOG_LEVEL_C_STR =
+    std::to_array<const char *>({"CRITICAL", "ERROR", "WARNING", "NOTICE", "INFO", "DEBUG", "TRACE"});
+
 LogLevel global_log_level = LogLevel::WARNING;
+
+namespace detail {
+
+void log(LogLevel level, const std::string & message) {
+    auto now = std::chrono::utc_clock::now();
+    auto formatted_message = std::format(
+        "{:%FT%TZ} {} {}",
+        std::chrono::time_point_cast<std::chrono::milliseconds>(now),
+        LOG_LEVEL_C_STR[static_cast<int>(level)],
+        message);
+    std::fputs(formatted_message.c_str(), stderr);
+}
+
+}  // namespace detail
--- netmount-server/logger.hpp
+++ netmount-server/logger.hpp
@@ -4,22 +4,24 @@
 #ifndef _LOGGER_HPP_
 #define _LOGGER_HPP_
 
-#include "utils.hpp"
-
-#include <array>
+#include <format>
 
 enum class LogLevel : int { CRITICAL, ERROR, WARNING, NOTICE, INFO, DEBUG, TRACE };
 
-constexpr auto LOG_LEVEL_C_STR =
-    std::to_array<const char *>({"CRITICAL", "ERROR", "WARNING", "NOTICE", "INFO", "DEBUG", "TRACE"});
-
 extern LogLevel global_log_level;
+
+namespace detail {
+
+void log(LogLevel level, const std::string & message);
+
+}
+
 
 template <typename... Args>
 void log(LogLevel level, std::format_string<Args...> fmt, Args &&... args) {
     if (level <= global_log_level) {
-        std::string formatted_string = ": " + std::vformat(fmt.get(), std::make_format_args(args...));
-        std::fputs((LOG_LEVEL_C_STR[static_cast<int>(level)] + formatted_string).c_str(), stderr);
+        auto message = std::vformat(fmt.get(), std::make_format_args(args...));
+        detail::log(level, message);
     }
 }
 
